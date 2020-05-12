7c47d1cb545d0784cce76ffed4ccda5cb4068a8d
digitalight <mikeglover06@gmail.com>
2020-05-12 18:11:49 +0100

    Move screen stuff to setting module
    Move the bgcolor and screen size to a seperate setting module for easier management

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,7 +7,10 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
     
     def run_game(self):
@@ -18,6 +21,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 if __name__ == '__main__':
