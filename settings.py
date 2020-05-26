ca44ea6abe213bee48014fa3231876a6d44eda87
Mike Glover <mikeglover06@gmail.com>
2020-05-27 00:16:27 +0100

    Added bullets

--- settings.py
+++ settings.py
@@ -10,3 +10,10 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 0.8
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 6
+        self.bullet_height = 30
+        self.bullet_color = (200, 30, 30)
+        self.bullets_allowed = 4
