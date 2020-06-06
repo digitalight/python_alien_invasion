c5db95eda21af824d10d2d39d2e37cb669592304
Mike Glover <mikeglover06@gmail.com>
2020-06-06 23:15:08 +0100

    Finished the game

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,12 @@
 import pygame.font
+from pygame.sprite import Group
 
 class Scoreboard:
     """A class to report scoring information"""
 
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
